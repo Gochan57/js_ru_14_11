4c19c79c0bb1028affba9693a9688c716b5f4521
Roman <kaboom.ua@gmail.com>
2016-12-18 20:58:11 +0200

    Update CommentsRoot.js

--- src/RouteHandlers/CommentsRoot.js
+++ src/RouteHandlers/CommentsRoot.js
@@ -5,13 +5,15 @@ class CommentRoot extends Component {
     static propTypes = {
 
     };
-
+    //возможно лучше хранить просто актуальный словарь
     static contextTypes = {
       language: PropTypes.string,
       dictionary: PropTypes.object
     }
 
     render() {
+        //общее замечание по всем местам: лучше в самих компонентах абстрагироваться от контекста, если он часто нужен. 
+        //Например сделав декоратор(по примеру connect) или, скажем, LocalizedText компонент по примеру Link
         return (
             <div>
                 <h1>{this.context.dictionary['Comments pagination'][this.context.language]}</h1>
